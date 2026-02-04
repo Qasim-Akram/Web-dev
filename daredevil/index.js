a3c6a2a66785c731c17402fcef25b18aa412888e
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-02-04 18:23:39 +0500

    some new things

---
+++ daredevil/index.js
@@ -0,0 +1,6 @@
+let errorparagraph=document.getElementById("error")
+
+function purchase(){
+    console.log("button clicked!")
+    errorparagraph.textContent="Something went wrong,Please try again later";
+}
--- welcomeback msg/script.js
+++ welcomeback msg/script.js
@@ -1,8 +1,21 @@
 let welcomeel=document.getElementById("welcome")
-
+let newel=document.getElementById("new")
+//for welcome 
 let name="M.Qasim Akram"
 let greet="Welcome back!"
 
 welcomeel.innerText = greet+" "+name
 
 welcomeel.innerText+="👍"
+
+//for new 
+let check="just checking prvious things "
+newel.innerText=check
+newel.innerText+="😂🥳❤️"
+
+
+function greetqasim(){
+    console.log(name + " "+ greet)
+}
+
+greetqasim()
