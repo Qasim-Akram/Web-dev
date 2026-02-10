a84cab94a77f4b0c4d3e83fa18d76fc651776e34
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-02-10 23:58:00 +0500

    fix basic

--- leadTracker/script.js
+++ leadTracker/script.js
@@ -1,22 +1,42 @@
-let myLeads=["ww.asdf.com","asdfgh"]
+let myLeads = []
 
-const textInput=document.getElementById("input-el")
-const saveBtn=document.getElementById("save-btn")
-const list=document.getElementById("showTrack")
+const textInput = document.getElementById("input-el")
+const saveBtn = document.getElementById("save-btn")
+const list = document.getElementById("showTrack")
 
-saveBtn.addEventListener("click",function(){
+saveBtn.addEventListener("click", function () {
     myLeads.push(textInput.value)
-    textInput.value=''
-    
-    
-
+    renderLead()
+    textInput.value = ''
 })
 
-for(let i=0;i<myLeads.length;i++){
-    //list.innerHTML+= "<li>"+myLeads[i]+"</li>"
-    let li=document.createElement("li")
-    li.textContent=myLeads[i]
-    list.append(li)
+let listItem = ''
+function renderLead() {
+    //listItem = "<li><a  target='_blank' href='"+textInput.value+"'>" + textInput.value + "</a></li>"
+    //templete strings 
+    listItem =
+        `<li>
+        <a  target='_blank' href='${textInput.value}'>
+        ${textInput.value}
+         </a>
+          </li>`
+    list.innerHTML += listItem
+    console.log(listItem)
+
+
 }
+
+// function renderLeads() {
+//     let listItems = ''
+//     for (let i = 0; i < myLeads.length; i++) {
+//         //list.innerHTML+= "<li>"+myLeads[i]+"</li>"
+//         //second method to create li element and append it to the list
+//         // let li=document.createElement("li")
+//         // li.textContent=myLeads[i]
+//         // list.append(li)
+//         listItems += "<li>" + myLeads[i] + "</li>"
+//     }
+//     list.innerHTML = listItems
+// }
 
 
