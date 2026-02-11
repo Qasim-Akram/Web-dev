9f0073f6bdc305e0347090ebc28e6c36b2307b87
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-02-11 14:53:58 +0500

    added delate all button

--- leadTracker/script.js
+++ leadTracker/script.js
@@ -2,40 +2,45 @@ let myLeads = []
 
 const textInput = document.getElementById("input-el")
 const saveBtn = document.getElementById("save-btn")
-const list = document.getElementById("showTrack")
+const  list = document.getElementById("showTrack")
+const deleteAllBtn = document.getElementById("deleteall-btn")
+const  leadFromStorage = JSON.parse(localStorage.getItem("Leads"))
+
+
+
+ if(leadFromStorage){
+     myLeads = leadFromStorage 
+     renderLead()
+}
+
 
 saveBtn.addEventListener("click", function () {
     myLeads.push(textInput.value)
+    localStorage.setItem("Leads",JSON.stringify(myLeads))
     renderLead()
     textInput.value = ''
 })
 
-let listItem = ''
-function renderLead() {
-    //listItem = "<li><a  target='_blank' href='"+textInput.value+"'>" + textInput.value + "</a></li>"
-    //templete strings 
-    listItem =
-        `<li>
-        <a  target='_blank' href='${textInput.value}'>
-        ${textInput.value}
-         </a>
-          </li>`
-    list.innerHTML += listItem
+deleteAllBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads = []
+    renderLead()
+    
+    
+})
 
 
-}
+ function renderLead() {
+    let listItems = " "
+     for (let i = 0; i < myLeads.length; i++) {
+         //list.innerHTML+= "<li>"+myLeads[i]+"</li>"
+         //second method to create li element and append it to the list
+         // let li=document.createElement("li")
+         // li.textContent=myLeads[i]
+         // list.append(li)
+         listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`
+    }
+    list.innerHTML = listItems
+    }
 
-// function renderLeads() {
-//     let listItems = ''
-//     for (let i = 0; i < myLeads.length; i++) {
-//         //list.innerHTML+= "<li>"+myLeads[i]+"</li>"
-//         //second method to create li element and append it to the list
-//         // let li=document.createElement("li")
-//         // li.textContent=myLeads[i]
-//         // list.append(li)
-//         listItems += "<li>" + myLeads[i] + "</li>"
-//     }
-//     list.innerHTML = listItems
-// }
-
-
+  
