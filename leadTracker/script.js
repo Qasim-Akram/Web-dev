b302f32aa094b7688fac048f98bf5584c4d74652
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-02-12 09:15:20 +0500

    final extension

--- leadTracker/script.js
+++ leadTracker/script.js
@@ -2,45 +2,47 @@ let myLeads = []
 
 const textInput = document.getElementById("input-el")
 const saveBtn = document.getElementById("save-btn")
-const  list = document.getElementById("showTrack")
+const list = document.getElementById("showTrack")
 const deleteAllBtn = document.getElementById("deleteall-btn")
-const  leadFromStorage = JSON.parse(localStorage.getItem("Leads"))
+const leadFromStorage = JSON.parse(localStorage.getItem("Leads"))
+const saveTabBtn = document.getElementById("save-tab")
 
 
 
- if(leadFromStorage){
-     myLeads = leadFromStorage 
-     renderLead()
+if (leadFromStorage) {
+    myLeads = leadFromStorage
+    render(myLeads)
 }
 
+function render(leads) {
+    let listItems = " "
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `<li><a target='_blank' 
+        href='${leads[i]}'>${leads[i]}</a></li>`
+    }
+    list.innerHTML = listItems
+}
 
 saveBtn.addEventListener("click", function () {
     myLeads.push(textInput.value)
-    localStorage.setItem("Leads",JSON.stringify(myLeads))
-    renderLead()
+    localStorage.setItem("Leads", JSON.stringify(myLeads))
+    render(myLeads)
     textInput.value = ''
 })
 
-deleteAllBtn.addEventListener("dblclick",function(){
+deleteAllBtn.addEventListener("dblclick", function () {
     localStorage.clear()
     myLeads = []
-    renderLead()
-    
-    
+    render(myLeads)
+    textInput.value = ''
 })
 
 
- function renderLead() {
-    let listItems = " "
-     for (let i = 0; i < myLeads.length; i++) {
-         //list.innerHTML+= "<li>"+myLeads[i]+"</li>"
-         //second method to create li element and append it to the list
-         // let li=document.createElement("li")
-         // li.textContent=myLeads[i]
-         // list.append(li)
-         listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`
-    }
-    list.innerHTML = listItems
-    }
 
-  
+saveTabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("Leads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
