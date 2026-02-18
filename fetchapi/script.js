54fdd430c1a31c17e8df0ce864049b8497fd9341
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-02-18 10:09:51 +0500

    api fetch and then

---
+++ fetchapi/script.js
@@ -0,0 +1,13 @@
+
+
+fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+        const imageElement = document.createElement('img')
+        imageElement.src = data.message
+        imageElement.alt = 'random dog picture'
+        document.getElementById('container').appendChild(imageElement) 
+
+
+
+    })
