b70c51c398f14e3df24bdbbd3f1f77b8d003c2b9
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-03-01 09:33:44 +0500

    new

--- thanku/script.js
+++ thanku/script.js
@@ -4,15 +4,16 @@ const sender = 'Hank'
 document.getElementById('content').innerHTML = getLabelHtml(sender, text, { name: "M.Qasim" }
     , { name: "M.Afnan" }
     , { name: "M.Abdullah" }
+    , {name:"Rehan"}
 )
 
 
 function getLabelHtml(sender, text, ...name) {
-    const labelHtml = name.map(devname => (`<div>
-             <p>Hi,${devname.name}</p>
+    return  name.map(devname => (`<div class="container">
+             <p>Hi, ${devname.name}</p>
              <p>${text}</p>
               <p>BestWishes,</p>
               <p>${sender}</P>
            </div>`))
-    return labelHtml
+    
 }
