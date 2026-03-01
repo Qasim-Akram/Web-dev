58c8f35101d73cd8d6ddccb371fede462fbb62b0
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-03-01 17:19:40 +0500

    nigga

--- Jsonapi/script.js
+++ Jsonapi/script.js
@@ -1,10 +1,16 @@
-try{
-    let  data = await fetch("https://apis.scrimba.com/jasoplaceholder/posts")
-    data = await
-    
+async function  fetchComment(){try{
+    const  response = await fetch("https://dummyjson.com/comments/150")
+    if(!response.ok){
+        throw new Error("there was a error with the api");
+    }
+    const  data = await response.json()
+    console.log(data)
 
 }catch(err){
-
+    console.log(err)
 }finally{
+ console.log("execution completed! ")
+}}
 
-}
+
+fetchComment()
