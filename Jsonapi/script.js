31b87a24869bd5eb1d9a6ddec9961eb833f93dac
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-03-02 10:52:08 +0500

    setting get and post

--- Jsonapi/script.js
+++ Jsonapi/script.js
@@ -1,16 +1,43 @@
-async function  fetchComment(){try{
-    const  response = await fetch("https://dummyjson.com/comments/150")
-    if(!response.ok){
-        throw new Error("there was a error with the api");
+async function sendpost() {
+    try {
+        const response = await fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+            title:'War Criminals',
+            body:'Usa and israel are war criminals',
+            userId:1
+            })
+        })
+        if (!response.ok) {
+            throw new Error("there was a error with the api");
+        }
+        const data = await response.json()
+        console.log(data)
+
+    } catch (err) {
+        console.log(err)
+    } finally {
+        console.log("execution completed! ")
     }
-    const  data = await response.json()
-    console.log(data)
+}
 
-}catch(err){
-    console.log(err)
-}finally{
- console.log("execution completed! ")
-}}
+async function getpost() {
+    try {
+        const response = await fetch("https://apis.scrimba.com/jsonplaceholder/posts/101")
+        if (!response.ok) {
+            throw new Error("there was a error with the api");
+        }
+        const data = await response.json()
+        console.log(data)
+
+    } catch (err) {
+        console.log(err)
+    }   finally {  
+        console.log("execution completed! ")
+    }}
 
 
-fetchComment()
+getpost()
